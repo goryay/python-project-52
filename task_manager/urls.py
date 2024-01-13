fad4c79a1f04f84a746e7c6659f7b7386f1d2df4
goryay <iam.goryn@gmail.com>
2024-01-13 16:35:23 +0300

    refactoring urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
-from .views import HomeView,
+from .views import HomeView
 
 
 # https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#the-set-language-redirect-view
@@ -27,3 +27,4 @@ urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('', HomeView.as_view(), name='home'),
     prefix_default_language=False,
+)
