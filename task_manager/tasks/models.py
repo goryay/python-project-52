090e60b5c369a5aca069e656012258fe9047edd3
goryay <iam.goryn@gmail.com>
2024-01-15 21:38:04 +0300

    adding new app-dir

---
+++ task_manager/tasks/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.urls import reverse_lazy
+
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
+from task_manager.users.models import UserModel
+from django.utils.translation import gettext_lazy as _
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=127,
+        unique=True,
+        blank=False,
+        error_messages={
+            'unique': _('Such a task already exists'),
+        },
+        help_text='<i>' + _('Required field') + '</i>',
+        verbose_name=_('Name')
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name=_('Description')
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        help_text='<i>' + _('Required field') + '</i>',
+        verbose_name=_('Status'),
+    )
+    labels = models.ManyToManyField(
+        Label, related_name='labels',
+        through='TaskLabelRelation',
+        blank=True,
+        verbose_name=_('Labels')
+    )
+    author = models.ForeignKey(
+        UserModel, related_name='author',
+        on_delete=models.PROTECT,
+        blank=False,
+        verbose_name=_('Author'),
+    )
+    executor = models.ForeignKey(
+        UserModel, related_name='executor',
+        on_delete=models.PROTECT,
+        blank=True, null=True,
+        verbose_name=_('Executor'),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Date of creation'),
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse_lazy('tasks', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ['created_at', 'name']
+
+
+class TaskLabelRelation(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, on_delete=models.PROTECT)
---
+++ task_manager/tasks/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from typing import Type
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    DetailView, CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django_filters.views import FilterView
+
+from task_manager.mixins import (
+    AddMessagesToFormSubmissionMixin,
+    CheckAuthorshipTaskMixin,
+    HandleNoPermissionMixin,
+)
+from task_manager.tasks.filters import TaskFilter
+from task_manager.tasks.forms import TaskForm
+from task_manager.tasks.models import Task
+
+
+class ListTasksView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    FilterView,
+):
+    model = Task
+    template_name = 'tasks/list.html'
+    context_object_name = 'tasks'
+    filterset_class: Type[TaskFilter] = TaskFilter
+
+    message_no_permission = _('You are not authorized')
+
+
+class ShowTaskView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    DetailView,
+):
+    model = Task
+    template_name = 'tasks/show.html'
+    context_object_name = 'task'
+
+    message_no_permission = _('You are not authorized')
+
+
+class CreateTaskView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    AddMessagesToFormSubmissionMixin,
+    CreateView,
+):
+    form_class = TaskForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('tasks:list')
+    extra_context = {
+        'title': _('Create task'),
+        'btn_name': _('Create'),
+    }
+
+    success_message = _('Task created successfully')
+    error_message = _('Error adding task')
+    message_no_permission = _('You are not authorized')
+
+    def form_valid(self, form):
+        """Add current user on author field Task model."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class UpdateTaskView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    AddMessagesToFormSubmissionMixin,
+    UpdateView,
+):
+    model = Task
+    form_class = TaskForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('tasks:list')
+    extra_context = {
+        'title': _('Changing a task'),
+        'btn_name': _('Change')
+    }
+
+    success_message = _('The task was successfully modified')
+    error_message = _('Task update error')
+    message_no_permission = _('You are not authorized')
+
+
+class DeleteTaskView(
+    HandleNoPermissionMixin,
+    CheckAuthorshipTaskMixin,
+    AddMessagesToFormSubmissionMixin,
+    DeleteView,
+):
+    model = Task
+    template_name = 'tasks/delete.html'
+    success_url = reverse_lazy('tasks:list')
+    extra_context = {
+        'title': _('Deleting a task'),
+        'btn_name': _('Yes, delete'),
+    }
+
+    success_message = _('The task was successfully deleted')
+    message_no_permission = _('Only the author can delete task')
