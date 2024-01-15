33f5c3bb41d7f4e9b2643733b659fbb49e2a7bb4
goryay <iam.goryn@gmail.com>
2024-01-15 22:02:39 +0300

    code update

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
-from .views import HomeView
+from .views import HomeView, UserLoginView, UserLogoutView
 
 
 # https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#the-set-language-redirect-view
@@ -26,5 +28,17 @@ urlpatterns = [
 ] + i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
     path('', HomeView.as_view(), name='home'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('users/', include('task_manager.users.urls')),
+    path('statuses/', include('task_manager.statuses.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
+    path('labels/', include('task_manager.labels.urls')),
     prefix_default_language=False,
 )
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
