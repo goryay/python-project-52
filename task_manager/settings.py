799fe032be124778ac4bc290e4c1ba0c55a2ff02
goryay <iam.goryn@gmail.com>
2024-01-15 21:32:57 +0300

    installed apps add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
+    'task_manager.users',
+    'task_manager.statuses',
+    'task_manager.tasks',
+    'task_manager.labels',
     'django_bootstrap5',
 ]
 
