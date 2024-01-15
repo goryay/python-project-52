2387a28c221592ca7b6a42f3f5c9c6c2472e3045
goryay <iam.goryn@gmail.com>
2024-01-15 21:46:27 +0300

    adding new app-dir statuses

---
+++ task_manager/statuses/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+
+class Status(models.Model):
+    name = models.CharField(
+        max_length=63,
+        unique=True, blank=False,
+        verbose_name=_('Name'),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('statuses', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
---
+++ task_manager/statuses/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+
+from task_manager.mixins import (
+    AddMessagesToFormSubmissionMixin,
+    HandleNoPermissionMixin,
+)
+from task_manager.statuses.forms import StatusForm
+from task_manager.statuses.models import Status
+
+
+class ListStatusesView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    ListView,
+):
+    model = Status
+    context_object_name = 'statuses'
+    template_name = 'statuses/list.html'
+
+
+class CreateStatusView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    AddMessagesToFormSubmissionMixin,
+    CreateView,
+):
+    form_class = StatusForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('statuses:list')
+    extra_context = {
+        'title': _('Create status'),
+        'btn_name': _('Create'),
+    }
+
+    success_message = _('Status successfully created')
+    error_message = _('Error creating status')
+    message_no_permission = _('Invalid action')
+
+
+class UpdateStatusView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    AddMessagesToFormSubmissionMixin,
+    UpdateView,
+):
+    model = Status
+    form_class = StatusForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('statuses:list')
+    extra_context = {
+        'title': _('Edit status'),
+        'btn_name': _('Edit'),
+    }
+
+    success_message = _('Status updated successfully')
+    error_message = _('Status update error')
+
+
+class DeleteStatusView(
+    HandleNoPermissionMixin,
+    LoginRequiredMixin,
+    AddMessagesToFormSubmissionMixin,
+    DeleteView
+):
+    model = Status
+    template_name = 'statuses/delete.html'
+    success_url = reverse_lazy('statuses:list')
+    extra_context = {
+        'title': _('Delete status'),
+        'btn_name': _('Yes, delete'),
+    }
+
+    success_message = _('Status successfully deleted')
+    error_message = _('Error deleting status')
+    message_no_permission = _('Only the author can delete status')
