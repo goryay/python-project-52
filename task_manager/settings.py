37873d378f1a27c4307429eef26fc1767487e248
goryay <iam.goryn@gmail.com>
2024-01-16 23:12:23 +0300

    make migrations

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v)_ps5ot5wsnr@x30+7viuc34)b22x4a$d!dmqncmsnu$aq60p'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'task_manager.tasks',
     'task_manager.labels',
     'django_bootstrap5',
-    'django_filtres',
+    'django_filters',
     'django_extensions',
 ]
 
