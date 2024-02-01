fa811a81981c23cc739845306dd046a91d4cdf61
goryay <iam.goryn@gmail.com>
2024-02-01 21:03:17 +0300

    settings refactoring

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,16 +125,18 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    DATABASES['default'].update(
+        dj_database_url.config(conn_max_age=600)
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-if not DEBUG:
-    DATABASES['default'].update(
-        dj_database_url.config(conn_max_age=600)
-    )
+#if not DEBUG:
+ #   DATABASES['default'].update(
+  #      dj_database_url.config(conn_max_age=600)
+   # )
 
 # End Database
 
